00742e2df19bd1a17fd694f428962c1a10482a10
Daniel Vedovato <842647@stud.unive.it>
2016-08-03 17:36:06 +0200

    Torch Relay Parser
    Parser of the torch relay from Rio2016 official site! The parser return a json with the current city and the cities where the torch will pass during the day (current day).

---
+++ parser_torch_relay.php
@@ -0,0 +1,29 @@
+<?php
+/**
+
+	Parser of the torch relay
+
+*/
+
+require "simple_html_dom.php";
+
+//vado a pescarmi la pagina
+$html = file_get_html('https://www.rio2016.com/en/olympic-torch-relay');
+
+$json_dati = array();
+
+//città corrente
+$h2_current_city = $html->find('h2[class=city-info--current-city]', 0);	
+$json_dati["current_city"] = trim($h2_current_city->plaintext);
+
+//città di oggi
+$ul_cities_list = $html->find('ul[class=cities-list]', 0);	
+
+$json_elenco_citta = array();
+foreach($ul_cities_list->find("li[class=cities-list--item]") as $li){
+	$json_elenco_citta[] = $li->plaintext;
+}
+
+$json_dati["today_cities"] = $json_elenco_citta;
+
+echo json_encode($json_dati);
